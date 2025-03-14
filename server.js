c13e580a1bb216c91a331a1336644d7985c02504
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-03-13 20:22:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 import express from 'express';
 const app = express();
 
+const PORT = 3000;
 
+
+//todo: i need route handlers 
+//? for what ?
+//* user needs to be able to submit email and message
+
+
+app.GET("/testing", (req, res) =>{
+    window.alert("endpoint defined!")
+
+});
+
+app.listen(PORT, () =>{
+    console.log(`\nserver listening on port:${PORT}`)
+});
