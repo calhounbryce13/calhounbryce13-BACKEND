2b21a6d02c22c8bbacdd8979fdc71088614cf445
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-03-15 22:39:52 -0700

    uodates

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from 'express';
 const app = express();
 import cors from 'cors';
 import { json } from 'express';
+import nodemailer from 'nodemailer';
 
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//!DEPLOYMENT NOTE: update the cors config. to allow access
+//!                 from the website domain
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin || origin === "null") {  // Allow requests from null origins
@@ -17,12 +24,24 @@ app.use(cors({
     methods: ['GET, POST']
 }))
 
-app.use(express.json());
+
+//! DEPLOYMENT NOTE: use environment variables stored on heroku dashboard
+//!                  prior to deployment, do not leave password in here
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth:{
+        user: "calhounbryce13@gmail.com",
+        pass: "T4113ngr10010137"
+    }
+})
 
 
 app.post("/testing", (req, res) =>{
     //! this endpoint will process the user data by sending 
     //! an email to myself, not collecting user email atm.
+    console.log(req.body);
     let testing = {message: JSON.stringify(req.body)};
     res.json(testing);
 
