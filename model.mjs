13a198f2f12bfec2fe4162ce062c728e0a7cae27
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-11-18 09:00:34 -0800

    backend web services for message logging and access

---
+++ model.mjs
@@ -0,0 +1,35 @@
+'use strict';
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+mongoose.connect(
+    process.env.CONNECTION_STRING,
+    { useNewUrlParser: true }
+);
+
+
+const messageSchema = new mongoose.Schema({
+    message: {type: String, required: true}
+});
+
+const Mssg = mongoose.model('new-message', messageSchema, 'messages');
+
+const db = mongoose.connection;
+
+
+const save_new_message = function(message){
+    const newMssg = Mssg({message:message});
+    return newMssg.save();
+}
+
+const get_all_messages = function(){
+    const messages = Mssg.find({});
+    return messages;
+}
+
+db.once("open", ()=>{
+    console.log("\nconnected to mongodb database!");
+});
+
+
+export default { save_new_message, get_all_messages }
---
+++ server.mjs
@@ -0,0 +1,54 @@
+/*
+Description: Server-side data processing script for the frontend of my portfolio page
+Author: Bryce Calhoun
+ */
+import express from 'express';
+import cors from 'cors';
+import messages from './model.mjs';
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "https://calhounbryce13.github.io",
+    methods: ['GET, POST']
+}));
+
+app.get('/messages', async(req, res) => {
+
+    try{
+        const data = await messages.get_all_messages();
+        res.status(200).json(data);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({"error":"issue getting the messages"});
+    }
+    return
+});
+
+
+app.post("/mailer", async(req, res) =>{
+    if(req.body){
+        if(req.body.message){
+            try{
+                messages.save_new_message(req.body.message);
+                res.status(200).json("success");
+            }catch(error){
+                console.log(error);
+                res.status(500).json({"error":"issue saving that message"});
+            }
+            return;
+        }
+        res.status(400).json({"error":"invalid request body"});
+        return;
+    }
+    res.status(400).json({"error":"missing request body"});
+    return;
+    
+});
+
+app.listen(PORT, () =>{
+    console.log(`\nserver listening on port:${PORT}`)
+});
